12cb6d88a3880a22eb9bcc381509b8f86908afd8
beeman <beeman@beeman.nl>
2025-11-12 19:38:50 +0400

    chore: add cloudflare workers config

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,12 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 import { resolve } from 'node:path'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    cloudflare(),
     nodePolyfills({}),
     react(),
     tailwindcss(),
