69c34f794819252b9b7029e73f610a1d554d3be7
beeman <beeman@beeman.nl>
2025-12-29 17:34:54 +0700

    chore: update logo

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,9 +1,15 @@
 export function AppFooter() {
   return (
-    <footer className="text-center p-2 bg-card/50 text-xs">
+    <footer className="flex justify-center items-center gap-1 p-2 bg-card/50 text-xs">
       Powered by{' '}
-      <a href="https://github.com/wallet-ui/wallet-ui" target="_blank" rel="noopener noreferrer">
-        🇼🇺 wallet-ui
+      <a
+        href="https://github.com/wallet-ui/wallet-ui"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center gap-1 font-bold hover:underline"
+      >
+        <img src="/wallet-ui.png" alt="Wallet UI Logo" className="size-4" />
+        <span>wallet-ui</span>
       </a>
     </footer>
   )
